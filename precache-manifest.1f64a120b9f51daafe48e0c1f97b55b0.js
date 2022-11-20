2c6d63b6abe26d67338e7624678495e304313f99
snigdh <snigdhkarki@gmail.com>
2022-11-20 11:58:08 +0545

    Updates

---
+++ precache-manifest.1f64a120b9f51daafe48e0c1f97b55b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ca4b33f2d051fa71d8f",
+    "url": "/React-Portfolio/static/css/main.14dcc64c.chunk.css"
+  },
+  {
+    "revision": "8ca4b33f2d051fa71d8f",
+    "url": "/React-Portfolio/static/js/main.8ca4b33f.chunk.js"
+  },
+  {
+    "revision": "93ef8a662a79e6e952e8",
+    "url": "/React-Portfolio/static/js/1.93ef8a66.chunk.js"
+  },
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "ebb898916356ba2b927dc23e805ae79f",
+    "url": "/React-Portfolio/index.html"
+  }
+];
